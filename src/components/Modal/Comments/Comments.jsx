5773efa9016a2a305c86f4aecd5cf6abc58665cd
mary-ue <maryandreeva100@gmail.com>
2023-09-21 20:17:42 +0300

    change Comments

--- src/components/Modal/Comments/Comments.jsx
+++ src/components/Modal/Comments/Comments.jsx
@@ -4,26 +4,32 @@ import PostTime from '../../Main/List/Post/PostTime';
 import style from './Comments.module.css';
 
 export const Comments = ({comments}) => {
-  console.log('comments', comments);
-  const {subreddit_id: id} = comments;
+  console.log('comments', comments.length);
 
   return (
-    <ul className={style.list}>
-      {comments?.map(comment => {
-        return (
-          <li key={id} className={style.item}>
-            <Text As='h3' className={style.author} size={18} tsize={22}>
-              {comment.author}
-            </Text>
-            <Text As='p' className={style.comment} size={14} tsize={18}>
-              {comment.body}
-            </Text>
-            {comment.created &&
-              <PostTime created={comment.created} />}
-          </li>
-        );
-      })}
-    </ul>
+    comments.length ? (
+      <ul className={style.list}>
+        {comments?.map(comment => {
+          return (
+            <li
+              key={`${comment.author_fullname}-${comment.subreddit_id}`}
+              className={style.item}
+            >
+              <Text As='h3' className={style.author} size={18} tsize={22}>
+                {comment.author}
+              </Text>
+              <Text As='p' className={style.comment} size={14} tsize={18}>
+                {comment.body}
+              </Text>
+              {comment.created &&
+                <PostTime created={comment.created} />}
+            </li>
+          );
+        })}
+      </ul>
+    ) : (
+      <Text As="p">Нет комментариев</Text>
+    )
   );
 };
 
