20bd8486ea1c9ae7760e3d9e5ac0c5e92c353cd6
mary-ue <maryandreeva100@gmail.com>
2023-10-03 15:06:42 +0300

    change window.location to navigate

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import {useDispatch} from 'react-redux';
 import {updateToken} from './store/tokenReducer';
 import {getToken} from './api/token';
 import {useEffect} from 'react';
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, useNavigate} from 'react-router-dom';
 
 function App() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = getToken();
+    dispatch(updateToken(token));
+  }, [dispatch]);
 
   useEffect(() => {
     const hashParams = new URLSearchParams(window.location.hash.substring(1));
     const accessToken = hashParams.get('access_token');
 
     if (accessToken) {
-      window.location.replace('/');
+      navigate('/');
     }
   }, []);
-
-  useEffect(() => {
-    const token = getToken();
-    dispatch(updateToken(token));
-  }, [dispatch]);
 
   return (
     <Routes>
